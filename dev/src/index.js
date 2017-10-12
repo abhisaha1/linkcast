1b2b587285fe092ef42fc78f724f4cb116c931e3
Abhishek Saha <abhisheksaha11@gmail.com>
2017-10-12 02:32:17 +0200

    removed dev

--- dev/src/index.js
+++
@@ -1,7 +0,0 @@
-import { app } from "hyperapp";
-import actions from "./actions";
-import state from "./state";
-import view from "./views/Main";
-import events from "./events";
-import logger from "@hyperapp/logger";
-app({ state, events, actions, view, mixins: [logger()] });
